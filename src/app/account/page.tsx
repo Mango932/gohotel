eaca572c4dc3a2e508d9bbae7306f97306045bac
Simontf15 <Stougasfryzuk@gmail.com>
2024-03-25 16:07:22 -0400

    small improvement

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -60,33 +60,24 @@ export default function account() {
     return (
         <div className="h-screen ">
             <Navbar />
-            <div className="relative"></div>
             <div className="flex flex-row overflow-hidden mt-10">
-                <div className="basis-1/3 mx-5">
-                    <div className="flex flex-col items-center mt-20 min-w-24">
-                        <div className="bg-secondary p-36 rounded-full">
-                            <PersonIcon className="scale-[800%]" />
-                        </div>
-                        <div className="p-4 items-center">
-                            <h2 className="w-full h-full text-center text-3xl text-neutral-800 dark:text-neutral-200">
-                                GoHotel Account
-                            </h2>
-                        </div>
+                <div className="flex flex-col basis-1/3 mx-5 items-center mt-32">
+                    <div className="bg-secondary p-36 rounded-full">
+                        <PersonIcon className="scale-[800%]" />
                     </div>
+                    <h2 className="w-full h-full text-center text-3xl text-neutral-800 dark:text-neutral-200 p-5">
+                        GoHotel Account
+                    </h2>
                 </div>
-                <div className="basis-2/3 mx-5">
-                    <div className="flex flex-col pt-10">
-                        <div className="p-4">
-                            <h2 className="font-bold text-2xl text-neutral-800 dark:text-neutral-200">
-                                My Bookings
-                            </h2>
-                        </div>
-                        <div className="w-full max-w-[1440px]">
-                            <DataTable
-                                columns={columns({ onDelete })}
-                                data={bookings}
-                            />
-                        </div>
+                <div className="flex flex-col basis-2/3 mx-5 pt-10">
+                    <h2 className="font-bold text-2xl text-neutral-800 dark:text-neutral-200 p-5">
+                        My Bookings
+                    </h2>
+                    <div className="w-full max-w-[1440px]">
+                        <DataTable
+                            columns={columns({ onDelete })}
+                            data={bookings}
+                        />
                     </div>
                 </div>
             </div>
