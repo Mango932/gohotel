39848843ec1160b15b4e2c99ea1a78e0f7661887
Simontf15 <Stougasfryzuk@gmail.com>
2024-03-30 14:07:44 -0400

    removed api

--- src/services/api_services.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-const api: AxiosInstance = axios.create({
-    baseURL: '/api',
-});
-
-export default api;
--- src/types/room.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const CreateBookingZod = z.object({
-    booking_id: z.number(),
-});
-
-export type CreateBookingDto = z.infer<typeof CreateBookingZod>; 
